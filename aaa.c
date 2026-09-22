cc4cddb68043cacbd4aecfd0d57f7b1812a87653
edi0oa <edioa922@gmail.com>
2026-09-22 16:37:45 +0900

    프라임 넘버 계산을 위한 새로운 파일추가

---
+++ aaa.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main(void) {
+    printf("Hello, World!\n");
+}
+
+
+
+
+
+
+// void main(void) {
+
+//     int a = 10;
+
+//     int b = 20, c;
+
+
+
+//     c = a + b;
+
+
+
+//     printf("%d, %d, %d\n", a, b, c);
+
+//     printf("The sum of %d and %d is %d\n", a, b, c);
+
+// }
